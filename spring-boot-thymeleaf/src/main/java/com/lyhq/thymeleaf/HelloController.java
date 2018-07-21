98b105031f5bcf87bc153e601b290eef4f2cf666
lyhq <947167863@qq.com>
2018-07-21 11:18:08 +0800

    spring-boot-thymeleaf修改

--- spring-boot-thymeleaf/src/main/java/com/lyhq/thymeleaf/HelloController.java
+++ spring-boot-thymeleaf/src/main/java/com/lyhq/thymeleaf/HelloController.java
@@ -1,4 +1,4 @@
-package com.neo.thymeleaf;
+package com.lyhq.thymeleaf;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @RequestMapping("/hello")
-    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
-        model.addAttribute("name", name);
-        return "hello";
-    }
+	@RequestMapping("/hello")
+	public String hello(Model model,
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		model.addAttribute("name", name);
+		return "hello";
+	}
 }
